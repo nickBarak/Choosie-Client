10327418256c7ec656a451257151b4d7ac6e3857
nickBarak <nicholasjbarak@gmail.com>
2020-07-05 11:05:46 -0700

    .

---
+++ build/precache-manifest.59f1c8f167a1c66a0813b223939cde8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eeb16ce9871cbadf23e73ce4e6d382a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ff4c74ec02c7d0a7c7a",
+    "url": "/static/css/main.de31f4a7.chunk.css"
+  },
+  {
+    "revision": "e8a7de8663240b63ff47",
+    "url": "/static/js/2.9243437b.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/static/js/2.9243437b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff4c74ec02c7d0a7c7a",
+    "url": "/static/js/main.30474081.chunk.js"
+  },
+  {
+    "revision": "8b1cba8d0a949dc8e9e4",
+    "url": "/static/js/runtime-main.9a65ebbf.js"
+  },
+  {
+    "revision": "7496b72dbb60da0e5e5f40c345d0d069",
+    "url": "/static/media/image-alt.7496b72d.png"
+  }
+]);

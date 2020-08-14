d205aff87b9cd3baf0df5ce2a46d15fc46f26b0f
nickBarak <nicholasjbarak@gmail.com>
2020-08-14 12:32:03 -0700

    .

--- src/APIs.js
+++ src/APIs.js
@@ -1,6 +1,6 @@
 export const server = new URL(
-    'https://www.choosie-api.herokuapp.com/'
-    // 'http://localhost:8082'
+    'https://choosie-api.herokuapp.com'
+    // 'http://127.0.0.1:8082'
 );
 
 export default { server };

f062a73b2668a5c2576c9f372e558ce3e204e379
nickBarak <nicholasjbarak@gmail.com>
2020-08-25 13:02:12 -0700

    .

--- src/APIs.js
+++ src/APIs.js
@@ -1,5 +1,5 @@
 export const server = new URL(
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV !== 'dev'
         ? 'http://choosie-env.eba-zrvb6h9m.us-west-1.elasticbeanstalk.com/'
         : 'http://localhost:8082'
 );

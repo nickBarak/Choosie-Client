e629780aacb6a63c2d2c6c8c867be80996ce87ca
nickBarak <nicholasjbarak@gmail.com>
2020-09-13 14:20:28 -0700

    .

--- src/store/actions/updateUser.action.js
+++ src/store/actions/updateUser.action.js
@@ -19,17 +19,21 @@ export const createUpdateUserLaunch = _ => ({ type: UPDATE_USER_LAUNCH }),
 export const updateUser = username => {
 	return dispatch => {
 		if (!username) {
+			console.log('Destroying session...');
 			dispatch(createUpdateUserFailure(null));
 			fetch(server + `destroy-session`, { credentials: 'include' })
+				.then(res => console.log('Session destroyed'))
 				.catch(e => console.log(e));
 			return;
 		}
+		console.log('Fetching user data...');
 		dispatch(createUpdateUserLaunch());
 		fetch(server + `users/${username}`, {
 			credentials: 'include'
 		})
 			.then(res => res.json())
 			.then(res => {
+				console.log('User data fetched successfully');
 				dispatch(createUpdateUserSuccess(res));
 			})
 			.catch(e => dispatch(createUpdateUserFailure(e)));
